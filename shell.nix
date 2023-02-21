fa8ed1abbedf77e5212198e2137b6718d9b04811
Oliver Sargison <sargo@sargo.cc>
2023-02-21 17:19:18 +1300

    Intial commit

---
+++ shell.nix
@@ -0,0 +1,25 @@
+with import <nixpkgs> { };
+
+mkShell rec {
+  # include any libraries or programs in buildInputs
+  nativeBuildInputs = [
+    pkg-config
+    cmake 
+    pkg-config 
+    freetype 
+    expat 
+    fontconfig
+  ];
+
+  buildInputs = [
+    udev alsa-lib vulkan-loader
+    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
+    libxkbcommon wayland # To use the wayland feature
+  ];
+  # shell commands to be ran upon entering shell
+  shellHook = ''
+  '';
+
+  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+  
+}
